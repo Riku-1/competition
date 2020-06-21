dec903fe8a7d51eb1c95370995e7f260db0be5a2
Riku-1 <osaka.riku@gmail.com>
2020-06-21 11:53:24 +0900

    init

---
+++ kotlin/src/abc081_a.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val input = readLine()
+    var count = 0
+
+    input?.forEach { if (it == '1') count++  }
+
+    println(count)
+}
---
+++ kotlin/src/abc081_b.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val n = readLine()
+    val an = readLine()!!.split(" ")
+    val _an = an.map { it.toInt() }
+
+    var loopn = 0
+    var __an = _an
+    var i = 0
+    while(i < 1000000000) {
+        if(__an.all { it % 2 == 0}) {
+            __an = __an.map { it/2 }
+            loopn++
+        } else {
+            break
+        }
+    }
+    println(loopn)
+}
