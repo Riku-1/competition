ca702f821c90406287627741b890282a9b8c7e69
Riku-1 <osaka.riku@gmail.com>
2020-06-23 22:26:11 +0900

    arc065_a

---
+++ kotlin/src/arc065_a_2.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val s = readLine()!!
+    var _s = s.reversed()
+
+    val slist = listOf("maerd", "remaerd", "esare", "resare")
+
+    do {
+        _s = fun(): String {
+            slist.forEach { if (_s.startsWith(it)) return _s.drop(it.length)}
+            return "false"
+        }()
+    }
+    while (!(_s == "" || _s == "false"))
+
+    if (_s == "") println("YES")
+    if (_s == "false") println("NO")
+}
