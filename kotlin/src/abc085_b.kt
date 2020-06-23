b16a32b49fc24f0670037528cca541b0b175d55b
Riku-1 <osaka.riku@gmail.com>
2020-06-23 20:50:46 +0900

    abc085_b

---
+++ kotlin/src/abc085_b.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val n = readLine()!!.toInt()
+    val d = arrayOfNulls<Int>(n)
+    for(i in 0 until n) {
+        d[i] = readLine()!!.toInt()
+    }
+    println(d.distinct().size)
+}
